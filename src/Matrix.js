5d7cb79443eab7957bfcf18521d3436b429096c4
Kane Koo <kanekoo@gmail.com>
2019-10-06 14:10:17 -0400

    First phase in case we need to clone down to PC/Ubuntu

--- src/Matrix.js
+++ src/Matrix.js
@@ -7,21 +7,30 @@ export default class Matrix extends Component {
 
   constructor() {
     super()
+    this.state = {
+      selectedColor: "#F00"
+    }
   }
 
   genRow = (vals) => (
-    vals.map((val, idx) => <Cell key={idx} color={val} />)
+    vals.map((val, idx) => <Cell key={idx} color={val} selectedColor={this.getSelectedColor} />)
   )
 
   genMatrix = () => (
     this.props.values.map((rowVals, idx) => <div key={idx} className="row">{this.genRow(rowVals)}</div>)
   )
 
+  setSelectedColor = (sColor) => {
+    this.setState( {selectedColor: sColor })
+
+  }
+  getSelectedColor = () => { this.state.selectedColor}
+
 
   render() {
     return (
       <div id="app">
-        <ColorSelector />
+        <ColorSelector setSelectedColor={this.setSelectedColor} />
         <div id="matrix">
           {this.genMatrix()}
         </div>
